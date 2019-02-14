97e010d8b3a217a42ad4fb603062259a354b08e8
Jimwel Aguinaldo <aguinaji@oregonstate.edu>
2019-02-13 23:29:12 -0800

    Read memory

--- psview.py
+++ psview.py
@@ -24,19 +24,23 @@ def LoadedModulesInProcess(proc):
 def ExecutablePagesInProcess(proc):
     return
 
-def MemoryInfo():
-    return
+def MemoryInfo(pid):
+    proc = psutil.Process(pid)
+    print(proc.memory_info())
 
 def main():
     parser = argparse.ArgumentParser(description="Defense Against the Dark Arts!")
     parser.add_argument("-p", "--enum_proc", help="enumerate all the running processes", action="store_true")
     parser.add_argument("-t", "--threads", type=int, metavar="pid", help="list all running threads within process boundry")
+    parser.add_argument("-m", "--mem_info", type=int, metavar="pid", help="Read the memory")
 
     args = parser.parse_args()
     if args.enum_proc:
         EnumRunningProcesses()
     elif args.threads:
         RunningThreadsInProcess(args.threads)
+    elif args.mem_info:
+        MemoryInfo(args.mem_info)
 
     
 if __name__ == "__main__":
