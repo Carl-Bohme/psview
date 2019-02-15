128412b898bba8f738fda77dae73afc77ce31331
Jimwel Aguinaldo <aguinaji@oregonstate.edu>
2019-02-14 16:05:34 -0800

    fix bugs

--- psview.py
+++ psview.py
@@ -1,14 +1,16 @@
 import argparse
 import psutil
+import os
 
 def EnumRunningProcesses():
     for proc in psutil.process_iter():
         try:
-            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'status', 'username'])
         except psutil.NoSuchProcess:
             pass
         else:
-            print(pinfo)
+            if pinfo['status'] == psutil.STATUS_RUNNING:
+                print(pinfo)
 
 def RunningThreadsInProcess(pid):
     proc = psutil.Process(pid)
@@ -18,8 +20,11 @@ def RunningThreadsInProcess(pid):
         print(thread)
     print('Number of threads: {}'.format(numThreads))
 
-def LoadedModulesInProcess(proc):
-    return
+def LoadedModulesInProcess(pid):
+    proc = psutil.Process(pid)
+    mem_map = proc.memory_maps()
+    for map in mem_map:
+        print(map)
 
 def ExecutablePagesInProcess(proc):
     return
@@ -32,6 +37,7 @@ def main():
     parser = argparse.ArgumentParser(description="Defense Against the Dark Arts!")
     parser.add_argument("-p", "--enum_proc", help="enumerate all the running processes", action="store_true")
     parser.add_argument("-t", "--threads", type=int, metavar="pid", help="list all running threads within process boundry")
+    parser.add_argument("-l", "--loaded_modules", type=int, metavar="pid", help="View loaded modules inside a process")
     parser.add_argument("-m", "--mem_info", type=int, metavar="pid", help="Read the memory")
 
     args = parser.parse_args()
@@ -39,6 +45,8 @@ def main():
         EnumRunningProcesses()
     elif args.threads:
         RunningThreadsInProcess(args.threads)
+    elif args.loaded_modules:
+        LoadedModulesInProcess(args.loaded_modules)
     elif args.mem_info:
         MemoryInfo(args.mem_info)
 
